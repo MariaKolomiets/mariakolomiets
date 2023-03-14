f17020a8bee498c6cee32f9141695f7fa0e0c928
Maria <maria.kolomiets.y@gmail.com>
2023-03-14 11:33:46 +0200

    дз1

---
+++ homework1/main.py
@@ -0,0 +1,6 @@
+from player import Character
+
+
+player1 = Character(name='Katia', year='2008',
+                    group='1',mark='11')
+print(player1)
---
+++ homework1/player.py
@@ -0,0 +1,37 @@
+import datetime
+from datetime import date
+
+class Character:
+    #Поля класа
+    name = ''
+    year = ''
+    group = ''
+    mark = ''
+    age = ''
+
+
+    #Конструктор
+    def __init__(self, name = '', year = '', group = '', mark = '',
+                 age = ''):
+        self.name = name
+        self.year = year
+        self.group = group
+        self.mark = mark
+        self.age = age
+
+    def get_age(self):
+        today = datetime.date.today().year
+        birth = int(self.year)
+        return \
+            f'{today - birth}\n'\
+
+    def __str__(self):
+        return self.get_ststs()
+
+    def get_ststs(self):
+        return \
+            f'Имя: {self.name}\n' \
+            f'год рождения: {self.year}\n' \
+            f'група: {self.group}\n' \
+            f'средний бал: {self.mark}\n' \
+            f'возраст: {self.age}\n'
